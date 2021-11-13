a8347278adfdb88d5f1bba1c85eddfa624f5ccc7
chuhx <chuhx1024@126.com>
2021-11-13 23:16:55 +0800

    temp

--- layouts/components/OriHeader.vue
+++ layouts/components/OriHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="clu-header" :class="{isMobile: isMobile}">
+    <div ref="header" class="clu-header" :class="{isMobile: isMobile}">
         <el-row type="flex" justify="space-around">
             <el-col :span="24">
                 <header class="clu-header-container type-page">
@@ -103,6 +103,8 @@ export default {
 
     mounted () {
         this.innerHeight = window.innerHeight
+        // this.scrollHandle()
+        window.addEventListener('scroll', this.touthToBottomhandle)
         this.showMenu = !this.isMobile
     },
 
@@ -186,11 +188,52 @@ export default {
                 this.showMenu = false
             }
         },
+        scrollHandle () {
+            // // 兼容性写法，该函数也是网上别人写的，不过找不到出处了，蛮好的，所有我也没有必要修改了
+            // // 判断鼠标滚轮滚动方向
+            // if (window.addEventListener) { // FF,火狐浏览器会识别该方法
+            //     window.addEventListener('DOMMouseScroll', wheel, false)
+            //     window.onmousewheel = document.onmousewheel = wheel// W3C
+            // }
+            // // 统一处理滚轮滚动事件
+            // function wheel (event) {
+            //     let delta = 0
+            //     if (!event) { event = window.event }
+            //     if (event.wheelDelta) { // IE、chrome浏览器使用的是wheelDelta，并且值为“正负120”
+            //         delta = event.wheelDelta / 120
+            //         if (window.opera) { delta = -delta }// 因为IE、chrome等向下滚动是负值，FF是正值，为了处理一致性，在此取反处理
+            //     } else if (event.detail) { // FF浏览器使用的是detail,其值为“正负3”
+            //         delta = -event.detail / 3
+            //     }
+            //     if (delta) { handle(delta) }
+            // }
+            // // 上下滚动时的具体处理函数
+            // function handle (delta) {
+            //     if (delta < 0) { // 向下滚动
+            //         console.log(123)
+            //         console.log(delta)
+            //     } else { // 向上滚动
+            //         console.log(345)
+            //     }
+            // }
+            // 页面滚动到底部触发的事件
+        },
+        touthToBottomhandle () {
+            // 滚动条滚动时，距离顶部的距离
+            const scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+            if (scrollTop === 0) {
+                this.$refs.header.style.backgroundColor = 'transparent'
+            } else {
+                this.$refs.header.style.backgroundColor = '#000'
+            }
+        },
     },
 }
 </script>
 <style lang='scss' scoped>
 .clu-header {
+    transition: 3s background-color;
+
     /deep/.el-menu.el-menu--horizontal {
         border-bottom: none;
         background-color: transparent;
