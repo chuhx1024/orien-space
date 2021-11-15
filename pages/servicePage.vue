2fed1ec83eccc1f2980be6d84b9c7c347ca19d8b
chuhx <chuhx1024@126.com>
2021-11-15 15:10:57 +0800

    temp

--- pages/servicePage.vue
+++ pages/servicePage.vue
@@ -22,17 +22,71 @@
         <div class="img-content">
             <img class="home-img" src="/img/service/service_0.jpg" alt="">
         </div>
-        <div class="img-content">
-            <img class="home-img" src="/img/service/service_1.jpg" alt="">
-            <div class="btn-left" @click="goToHash('productPage', 'numberSecond')" />
+        <div class="img-container" style="background-color: #040a17; padding: 148px 0;">
+            <img class="wallpaper left" src="/img/service/service_11.jpg" border="1" alt="">
+            <section class="text-wallpaper">
+                <section class="text-content type-page">
+                    <div class="main-title-0">
+                        使命必达
+                    </div>
+                    <div class="text-desc">
+                        通过多级助推，将载荷准确送至太空预定轨道
+                    </div>
+                    <div class="main-title-1">
+                        灵活发射
+                    </div>
+                    <div class="text-desc">
+                        通过陆地、海洋等多种方式灵活发射可实现海上自主回收
+                    </div>
+                    <div class="text-btn">
+                        <el-button type="primary" plain @click="gotoMail('bd@orienspace.com')">
+                            预约发射
+                        </el-button>
+                    </div>
+                </section>
+            </section>
         </div>
-        <div class="img-content">
-            <img class="home-img" src="/img/service/service_2.jpg" alt="">
-            <div class="btn-left btn-right" @click="goToHash('productPage', 'numberSecond')" />
+        <div class="img-container" style="background-color: #000; padding: 45px 0;">
+            <img class="wallpaper right" src="/img/service/service_22.jpg" alt="">
+            <section class="text-wallpaper">
+                <section class="text-content type-page text-align-right">
+                    <div class="main-title-0">
+                        商业合作
+                    </div>
+                    <div class="text-desc-2">
+                        你想与我们一起迈向太空吗？
+                    </div>
+                    <div class="text-desc-3">
+                        尊敬的企业及个人客人：我们的箭体可提供整体/分部品牌广告合作及其他营销服务，请您联系我们洽谈具体方案
+                    </div>
+                    <div class="text-btn">
+                        <el-button type="primary" plain @click="gotoMail('bd@orienspace.com')">
+                            联系我们
+                        </el-button>
+                    </div>
+                </section>
+            </section>
         </div>
-        <div class="img-content">
-            <img class="home-img" src="/img/service/service_3.jpg" alt="">
-            <div class="btn-left" @click="goToHash('productPage', 'numberSecond')" />
+        <div class="img-container">
+            <img class="wallpaper" src="/img/service/service_3.jpg" alt="">
+            <section class="text-wallpaper">
+                <section class="text-content type-page">
+                    <div class="main-title">
+                        想要近距离观看发射火箭?
+                    </div>
+                    <div class="sub-title">
+                        引力-1号 发射倒计时
+                    </div>
+                    <div class="time-text">
+                        2023年6月31日
+                    </div>
+                    <div class="text-btn">
+                        <el-button type="primary" plain @click="gotoMail('bd@orienspace.com')">
+                            预约观看
+                        </el-button>
+                    </div>
+                </section>
+            </section>
         </div>
         <div class="carouser-content">
             <carousel
@@ -60,8 +114,9 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapState } from 'vuex'
 import BannerComSmall from '../components/BannerComSmall.vue'
+import { gotoMail } from '@/utiles/tool.js'
 export default {
     name: 'ServicePage',
     components: { BannerComSmall },
@@ -78,7 +133,7 @@ export default {
         ...mapState('app', ['isMobile']),
     },
     methods: {
-        ...mapActions('app', ['setIsMobile']),
+        gotoMail,
     },
 }
 </script>
@@ -104,25 +159,110 @@ export default {
         }
 
         .img-content {
-            position: relative;
-
-            .btn-left {
-                position: absolute;
-                top: 33%;
-                left: 7%;
-                width: 31%;
-                height: 50%;
-                cursor: pointer;
-            }
-
-            .btn-right {
-                left: 64%;
-                top: 30%;
-            }
-
             .home-img {
                 width: 100%;
                 background-size: cover;
+            }
+        }
+
+        .img-container {
+            position: relative;
+            line-height: 1;
+
+            .wallpaper {
+                width: 100%;
+                background-size: cover;
+
+                &.left {
+                    width: 50%;
+                    transform: translateX(98%);
+                    border-style: dashed;
+                    border-color: #fff;
+                }
+
+                &.right {
+                    width: 50%;
+                    transform: translateX(20%);
+                }
+            }
+
+            .text-wallpaper {
+                height: 100%;
+                position: absolute;
+                top: 0;
+                left: 0;
+                bottom: 0;
+                right: 0;
+
+                .text-content {
+                    height: 100%;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    text-align: left;
+
+                    .main-title {
+                        font-size: 33px;
+                        color: #fff;
+                        margin-bottom: 76px;
+                    }
+
+                    .main-title-0 {
+                        font-size: 33px;
+                        color: #fff;
+                        margin-bottom: 16px;
+                    }
+
+                    .main-title-1 {
+                        font-size: 33px;
+                        color: #fff;
+                        padding: 100px 0 16px;
+                    }
+
+                    .sub-title {
+                        font-size: 16px;
+                        margin-bottom: 15px;
+                    }
+
+                    .time-text {
+                        font-size: 36px;
+                        font-weight: 700;
+                        color: #fff;
+                    }
+
+                    .text-desc {
+                        width: 300px;
+                        font-size: 16px;
+                        color: #c9caca;
+                        line-height: 24px;
+                    }
+
+                    .text-desc-2 {
+                        font-size: 16px;
+                        color: #c9caca;
+                        line-height: 24px;
+                    }
+
+                    .text-desc-3 {
+                        width: 450px;
+                        font-size: 13px;
+                        color: #c9caca;
+                        padding-top: 30px;
+                        line-height: 18px;
+                    }
+
+                    .text-btn {
+                        padding-top: 66px;
+                    }
+                }
+
+                .text-align-right {
+                    text-align: right;
+
+                    .text-desc-3 {
+                        align-self: flex-end;
+                    }
+                }
             }
         }
 
